181cf6a1a809a78eafdf8a75493f9622183c93d4
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2023-04-11 21:11:04 +0100

    add-layout

--- src/components/portfolio-details/modal-details.js
+++ src/components/portfolio-details/modal-details.js
@@ -40,34 +40,34 @@ export const modalDetails = [
         image: [
           {
             slide:
-              "https://i.ibb.co/vkvSkHC/screencapture-localhost-3001-parent-dashboard-my-wards-2023-04-09-15-06-52.png",
-            label: "Ward List",
+              "https://i.ibb.co/WKZfvKJ/screencapture-cbt-flavetechs-candidate-answers-2023-04-09-20-41-18-1.png",
+            label: "Candidate Exam Page",
             description:
-              "A list of wards/children under a parent in the Parent's portal",
+              "Answering of questions by candidates",
           },
           {
             slide:
-              "https://i.ibb.co/gJcZDdJ/screencapture-localhost-3001-stds-dashboard-student-student-note-2023-04-09-15-03-32.png",
-            label: "Student Notes",
+              "https://i.ibb.co/jTsMgKT/screencapture-cbt-flavetechs-candidate-view-answers-2023-04-09-20-42-43.png",
+            label: "Answer View",
             description:
-              "A list of notes written by a student in the Student Portal",
+              "List of answers by candidates",
           },
+        
           {
             slide:
-              "https://i.ibb.co/mykgTNL/screencapture-localhost-3001-dashboard-smp-session-edit-session-class-table-2023-04-09-15-49-32.png",
-            label: "Session Subject Edit",
+              "https://i.ibb.co/gWVJ70c/Add-question.png",
+            label: "Question Edit",
             description:
-              "Subjects available in a session being edited in the Staff/Teachers Portal ",
+              "Editing Of questions by Examiners",
           },
         ],
         category: "Web Design",
         client: "Flavetech",
         date: "01, May 2022",
         projectDetails:
-          "The school management portal  facilitates the management of various operations within educational institutions such as schools, colleges, and universities. It is designed to streamline administrative tasks and improve communication among administrators, teachers, students, and parents. It also has a feature that allows users switch themes as it suits them.",
-      featureList:[{feature:"Student Information Management:  This provides students with a comprehensive platform to manage and store important information, such as assessments, notes, announcements, class and exam timetables, and result printing. This allows students to easily access and organize their academic information in one place, ensuring they have the necessary resources and updates for their studies."},
-    {feature:"Teacher and Staff Management: The portal enables administrators to manage teacher and staff information, student information, parent information, session,class and subject setups, class and exam schedules, attendance,admissions, performance storage and evaluations."},
-    {feature:"Parent and Guardian Management: It aims to provide parents or guardians with convenient access to information, communication channels, and features that enable them to actively participate in their children's education and stay engaged with the school."}]
+          "The computer basted test provides efficient method for administering tests and assessments in various settings, offering convenience, flexibility, and data-driven insights for educators, administrators, and test-takers alike.",
+      featureList:[{feature:"Examiner Section:  This is designed to facilitate the creation, administration, and analysis of tests by examiners. It includes features such as test creation and customization, test administration and scheduling, test customization and reusability, and user management, to support examiners in effectively managing the testing process."},
+    {feature:"Candidate Section: This is designed for test-takers to take tests or exams. It includes features such as test login and authentication, test navigation and instructions, question presentation, test progress and timer, response submission, test feedback, and accessibility features, resources to provide a user-friendly and efficient testing experience for test-takers."},]
         },
 
 
--- src/components/portfolio-details/portfolio-list.js
+++ src/components/portfolio-details/portfolio-list.js
@@ -1,5 +1,5 @@
 export const portfolioList = [
     {images:'https://i.ibb.co/Kr3fXKX/smp-webpage-2.png',title:'School Management Portal'},
-    {images:'https://i.ibb.co/Kr3fXKX/smp-webpage-2.png',title:'Computer Based Test'},
+    {images:'https://i.ibb.co/SV9kGFW/screencapture-cbt-flavetechs-examiner-dashboard-sms-exam-list-2023-04-09-20-00-54.png',title:'Computer Based Test'},
     {images:'https://i.ibb.co/Kr3fXKX/smp-webpage-2.png',title:'Flavetech Web Services'}
 ]
